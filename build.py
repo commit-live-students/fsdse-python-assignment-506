c28d264296552f8bfb46091cf040c2999374f0e9
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-14 03:03:09 +0000

    Done

--- build.py
+++ build.py
@@ -7,19 +7,37 @@ def load_data():
     """
     Enter your code here
     """
-
+    df = pd.read_csv('./files/titanic.csv')
+    return df.columns
 
 def draw_plot(titanic):
     """
     Enter your code here
     """
+    counts = titanic['Sex'].value_counts()
+    proportions = counts.apply(lambda x: round((float(x)/counts.sum())*100.1))
+    exp = (0.1, 0)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(list(props), explode=explode, labels=tuple(props.index.tolist()))
+    ax1.axis('equal')  
+    plt.title('Sex Proportion')
+    plt.show()
 
 
 def draw_historgram(titanic):
     """
     Enter your code here
     """
+    data = titanic['Fare'].sort_values(ascending=False)
+    plt.hist(data, 50)
+    plt.title('Fair Paid Histogram')
+    plt.xlabel('Fare')
+    plt.ylabel('Frequency')
+    plt.show()
 
 
-# titanic = load_data()
-# draw_historgram(titanic)
+
+titanic = load_data()
+print(titanic)
+draw_plot(titanic)
+draw_historgram(titanic)
