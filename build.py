35fb426b6b99a53fbb2ec4942e7fa611a1499830
ramkrishan-sahu <ram04190@gmail.com>
2017-07-11 14:51:29 +0000

    Done

--- build.py
+++ build.py
@@ -4,21 +4,32 @@ import numpy as np
 
 
 def load_data():
-    """
-    Enter your code here
-    """
-
+    global df
+    df = pd.read_csv("/home/ram/Desktop/Boks/titanic.csv")
+    df = pd.DataFrame(df)
+    return df
 
 def draw_plot(titanic):
-    """
-    Enter your code here
-    """
+    male = df.Sex.value_counts().male
+    female = df.Sex.value_counts().female
 
+    labels = ['Male', 'Female']
+    proportions = [male, female]
+    explode = (0, 0.1)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', explode=explode)
+    plt.title("Sex Proportion")
+    plt.show()
+    return
 
 def draw_historgram(titanic):
-    """
-    Enter your code here
-    """
+    fares = df.Fare.sort_values()
+    plt.hist(fares)
+    plt.title("Fare Payed Histogram")
+    plt.xlabel("Fare")
+    plt.ylabel("Frequency")
+    plt.show()
+    return
 
 
 # titanic = load_data()
