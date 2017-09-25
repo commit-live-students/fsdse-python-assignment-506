ec542dc2963f33fb86ee01f68a71805a620b2df8
Keyur Bhole <kyrbhole@gmail.com>
2017-09-25 21:35:55 +0530

    Done

--- build.py
+++ build.py
@@ -4,21 +4,33 @@ import numpy as np
 
 
 def load_data():
-    """
-    Enter your code here
-    """
-
+    titanic = pd.read_csv('titanic.csv')
+    return titanic
 
 def draw_plot(titanic):
-    """
-    Enter your code here
-    """
-
+    proportions = []
+    sum_instances = titanic['Sex'].value_counts()
+    length = len(titanic['Sex'])
+    proportions = list(sum_instances)
+    labels = ['Males','Females']
+    explode = (0,0.1)
+    sizes = proportions
+    fig, ax1 = plt.subplots(figsize = (6,6))
+    ax1.pie(sizes, explode = explode, labels = labels, shadow = True, startangle=90)
+    ax1.axis('equal')
+    ax1.set_title("Sex Proportions")
+    plt.show()
 
 def draw_historgram(titanic):
-    """
-    Enter your code here
-    """
+    sort_fare = titanic.sort_values('Fare')
+    bins = np.arange(5,85)
+    fig, axis = plt.subplots(figsize=(10,6))
+    axis.hist(titanic['Fare'],bins=bins)
+    axis.set_title("Fare Pay Histogram")
+    axis.set_xlabel("Fare")
+    axis.set_ylabel("Frequency")
+    plt.tight_layout()
+    plt.show()
 
 
 # titanic = load_data()
